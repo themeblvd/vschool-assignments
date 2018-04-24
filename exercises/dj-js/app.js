745f60570c6542d59e583eba393b227cd2c6a782
Jason Bobich <jasonbobich@gmail.com>
2018-04-24 11:35:34 -0600

    Add dj-js

---
+++ exercises/dj-js/app.js
@@ -0,0 +1,37 @@
+( function() {
+
+    /*
+     * The square's color will change as follows:
+     *
+     * 1. Blue when the mouse hovers over the square
+     * 2. Red when the mouse button is held down over the square
+     * 3. Yellow when the mouse button is let go over the square
+     * 4. Green when the mouse is double clicked in the square
+     * 5. Orange when the mouse scroll is used somewhere in the window (not just over the square).
+     *
+     * @link https://coursework.vschool.io/dj-js/
+     */
+
+    var square = document.getElementById( 'square' );
+
+    square.addEventListener( 'mouseenter', function( event ) {
+        event.target.style.backgroundColor = 'blue';
+    } );
+
+    square.addEventListener( 'mousedown', function( event ) {
+        event.target.style.backgroundColor = 'red';
+    } );
+
+    square.addEventListener( 'mouseup', function( event ) {
+        event.target.style.backgroundColor = 'yellow';
+    } );
+
+    square.addEventListener( 'dblclick', function( event ) {
+        event.target.style.backgroundColor = 'green';
+    } );
+
+    document.addEventListener( 'scroll', function( event ) {
+        square.style.backgroundColor = 'orange';
+    } );
+
+} )();
