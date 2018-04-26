1b837287d9c986d359c3b8f44b8ea084eb64bda7
Jason Bobich <jasonbobich@gmail.com>
2018-04-26 15:51:23 -0600

    Add for-within-for-within-for

---
+++ exercises/for-within-for-within-for/app.js
@@ -0,0 +1,22 @@
+// Using a for loop within a for loop create an array that looks
+// like this when printed to the console:
+// ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+var arr = [];
+
+for ( var i = 0; i < people.length; i++ ) {
+
+    arr.push( people[ i ] + ':' );
+
+    for ( var j = 0; j < alphabet.length; j++ ) {
+
+        arr.push( alphabet[ j ].toUpperCase() );
+
+    }
+}
+
+console.log( arr );
