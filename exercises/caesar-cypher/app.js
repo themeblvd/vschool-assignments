1f8c3f7a7484ca3beb4666f43145f72aab9fa349
Jason Bobich <jasonbobich@gmail.com>
2018-04-30 11:22:06 -0600

    Add caesar-cypher

---
+++ exercises/caesar-cypher/app.js
@@ -0,0 +1,29 @@
+var readline = require('readline-sync');
+
+var input = readline.question('What phrase would you like to encrypt?: ');
+
+var shift = parseInt(readline.question('How many letters would you like to shift?: '));
+
+console.log( cypher( input, shift ) );
+
+function cypher( str, shift ) {
+
+    var abc = 'abcdefghijklmnopqrstuvwxyz';
+
+    var encoded = '';
+
+    str = str.toLowerCase();
+
+    for ( let i = 0; i < str.length; i++ ) {
+
+        let currentIndex = abc.indexOf( str[i] );
+
+        if ( currentIndex > -1 ) {
+            encoded += abc[ (currentIndex + shift) % abc.length ];
+        } else {
+            encoded += str[i];
+        }
+    }
+
+    return encoded;
+}
