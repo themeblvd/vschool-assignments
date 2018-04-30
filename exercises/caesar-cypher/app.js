413ff8fd3190736cc7e0c090ec8f415e178d5b02
Jason Bobich <jasonbobich@gmail.com>
2018-04-30 11:26:13 -0600

    Add back .gitignore after deleting node_modules

--- exercises/caesar-cypher/app.js
+++ exercises/caesar-cypher/app.js
@@ -1,7 +1,5 @@
 var readline = require('readline-sync');
-
 var input = readline.question('What phrase would you like to encrypt?: ');
-
 var shift = parseInt(readline.question('How many letters would you like to shift?: '));
 
 console.log( cypher( input, shift ) );
