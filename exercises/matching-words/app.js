b8e1e48f8dc673ea21ddd3577e4672051d821af6
Jason Bobich <jasonbobich@gmail.com>
2018-05-09 10:49:48 -0600

    Add matching-words

---
+++ exercises/matching-words/app.js
@@ -0,0 +1,15 @@
+function duplicates(str){
+
+    return str.split(' ')
+        .filter(function(word, i, all){ // Get all occrences of duplicates, i.e. ['foo', 'foo'].
+            return i != all.indexOf(word);
+        })
+        .filter(function(word, i, all){ // Get a unique list of those duplicate occrences, i.e. ['foo'].
+            return i == all.indexOf(word);
+        });
+
+}
+
+console.log(duplicates('foo bar baz foo foo foo')); // ["foo"]
+
+console.log(duplicates("Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas."));
